a0b4c1a24950f492a5d15bfedd3535bfa3e0b080
mkotelnikov <maksim.kotelnikov@soft-control.ru>
2026-08-08 10:26:09 +0500

    feat: add routes]

---
+++ src/app/routes/routes.tsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from 'react-router';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      {
+        index: true,
+        // Component: Layout,
+        element: <a>Main</a>,
+      },
+      {
+        path: 'community',
+        element: <a>Community</a>,
+      },
+      {
+        path: ':category',
+        element: <a>Category</a>,
+      },
+      {
+        path: 'article',
+        children: [
+          {
+            path: ':alias',
+            element: <a>Article</a>,
+          },
+        ],
+      },
+      {
+        path: 'profile',
+        children: [
+          {
+            index: true,
+            element: <a>Profile</a>,
+          },
+          {
+            path: 'edit',
+            element: <a>Edit Profile</a>,
+          },
+        ],
+      },
+    ],
+  },
+]);
